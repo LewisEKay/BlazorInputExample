3f1ce03d070b0a94c0df29872799e0837091c933
Lewis <lewisedwardkay@outlook.com>
2021-07-27 16:01:10 +0100

    init

---
+++ WebApplication1/WebApplication1/Client/MyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Client
+{
+    public class MyModel
+    {
+        [MaxLength(3)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
